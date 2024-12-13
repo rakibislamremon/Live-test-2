1090321af5331896a159bc8e4d3f8d551384c205
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-13 22:30:45 +0600

    2nd problem solve

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 for (let i = 2;i <= 20;i += 2){
     console.log(i);
 }
+
+
+/* 2nd Problem */
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString
+    ("Rakibul Islam"));
